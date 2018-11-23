6e8c1e6a115de5a35e956883ae78655949139569
feranco <feranco@gmail.com>
2018-11-23 18:01:45 +0100

    add longest increasing subseq with return path

--- longestIncreasingSubsequence/main.cpp
+++ longestIncreasingSubsequence/main.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 
 int main() {
-  //list<int> sequence{0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
-  list<int> sequence{9,6,1,5,3,7,55,12};
+  list<int> sequence{0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
+  //list<int> sequence{9,6,1,5,3,7,55,12};
   auto result = longestIncreasingSubsequence(sequence);
   for (int x : result) std::cout << x << " ";
 }
