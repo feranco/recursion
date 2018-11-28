7c8f9337587e23aedef59d82793264e12900a033
feranco <feranco@gmail.com>
2018-11-28 08:54:29 +0100

    fixed parenthesized expressions

--- parenthesizeExpression/parenthesizeExpression.cpp
+++ parenthesizeExpression/parenthesizeExpression.cpp
@@ -2,16 +2,15 @@
 
 vector<string> parenthesizeExpression (const string& expr) {
 
-  if (expr.size() == 2) return vector<string>{"("+expr+")"};
   if (expr.size() == 1) return vector<string>{expr};
 
   vector<string> result;
 
-  for (size_t len = 0; len < expr.size()-1; ++len) {
-    auto l = parenthesizeExpression(expr.substr(0, len+1));
-    auto r = parenthesizeExpression(expr.substr(len+1));
-    for (auto& s1 : l) {
-      for (auto& s2 : r) {
+  for (size_t len = 1; len < expr.size(); ++len) {
+    auto left = parenthesizeExpression(expr.substr(0, len));
+    auto right = parenthesizeExpression(expr.substr(len)); //substr without 2nd argument mean up to the end of the string
+    for (auto& s1 : left) {
+      for (auto& s2 : right) {
 	result.emplace_back("("+s1+s2+")");
       }
     }
