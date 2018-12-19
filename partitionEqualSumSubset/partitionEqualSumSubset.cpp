105a8e505e2bb434190fc0364d0274314031a9d5
feranco <feranco@gmail.com>
2018-12-19 09:56:35 +0100

    add partition equal sum subsets

--- partitionEqualSumSubset/partitionEqualSumSubset.cpp
+++ partitionEqualSumSubset/partitionEqualSumSubset.cpp
@@ -1,7 +1,12 @@
 #include "partitionEqualSumSubset.h"
 #include <algorithm>
 
-//O(k!) time nd O(n) space
+//O(k!*k^(n-k)) time and O(n) space, where n is nums.size()
+//the time complexity might have been O(k^n) without the line if (subsetSum[j] == 0) break;
+//with this line the for loop (and the recursive call) is executed once for nums[0],
+//twice for nums[1], k times for nums[k-1] and this is equal to k!
+//for nums[k], ..., nums[n-1] the for loop is executed always k times and this is equal to k^(n-k)
+//because k is the branching factor and n-k is the deep
 bool canPartitionKSubsets(const vector<int>& nums, size_t idx, int maxSum, vector<int>& subsetSum) {
         
   if (idx == nums.size()) return true;
@@ -14,8 +19,7 @@ bool canPartitionKSubsets(const vector<int>& nums, size_t idx, int maxSum, vecto
       }
     }
     subsetSum[j] -= nums[idx];
-    //this line avoid to place a number in an empty subset multiple time
-    //saving repeated work
+    //this line avoid to place a number in an empty subset multiple time saving repeated work
     if (subsetSum[j] == 0) break;
   }
         
