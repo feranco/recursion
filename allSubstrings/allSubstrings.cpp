a4c88df3598a575b1c408973c066bb4c9ca9dc4e
Fernando <feranco@gmail.com>
2019-04-02 09:29:01 +0200

    all substrings

---
+++ allSubstrings/allSubstrings.cpp
@@ -0,0 +1,11 @@
+#include <string>
+
+using std::string;
+
+vector<string> allSubstrings (const std::string& s) {
+  if (s.size() == 0) s.push_back();
+  std::vector<int> f;
+  f.push_back(3);
+  std::string s1;
+  s1.append
+}
